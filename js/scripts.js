9821e35081d9c8f13ef86dfdbc355c8674851e90
Aymeric <aymericbertrand.pro@gmail.com>
2021-04-26 12:06:19 +0200

    Added reset and erase function

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,63 @@
-function add(myArray) {
-    return myArray.reduce((x, y) => x + y, 0);
-}
-
-function substract(myArray) {
-    return myArray.reduce((x, y) => x - y, 0);
-}
-
-function multiply(myArray) {
-    return myArray.reduce((x, y) => x * y, 1);
-}
-
-function divide(myArray) {
-    return myArray.reduce((x, y) => x / y);
-}
-
-function operate(x, operator, y) {
-    if (operator === "+") {
-        add(x, y);
-    }
-    else if (operator === "-") {
-        substract(x, y);
-    }
-    else if (operator === "*") {
-        multiply(x, y);
-    }
-    else if (operator === "/") {
-        divide(x, y);
-    }
-}
-
-function defineDigitsListener() {
-    const digits = document.querySelectorAll(".digit");
+function init() {
     const display = document.querySelector(".display__text");
-    digits.forEach((digit) => {
-        const digitValue = digit.getAttribute("value");
-        digit.addEventListener('click', () => {
-            display.textContent += digitValue;
+
+    function add(myArray) {
+        return myArray.reduce((x, y) => x + y, 0);
+    }
+
+    function substract(myArray) {
+        return myArray.reduce((x, y) => x - y, 0);
+    }
+
+    function multiply(myArray) {
+        return myArray.reduce((x, y) => x * y, 1);
+    }
+
+    function divide(myArray) {
+        return myArray.reduce((x, y) => x / y);
+    }
+
+    function operate(x, operator, y) {
+        if (operator === "+") {
+            add(x, y);
+        }
+        else if (operator === "-") {
+            substract(x, y);
+        }
+        else if (operator === "*") {
+            multiply(x, y);
+        }
+        else if (operator === "/") {
+            divide(x, y);
+        }
+    }
+
+    function defineDigitsListener() {
+        const digits = document.querySelectorAll(".digit");
+        digits.forEach((digit) => {
+            const digitValue = digit.getAttribute("value");
+            digit.addEventListener('click', () => {
+                display.textContent += digitValue;
+            });
         });
-    });
+    }
+
+    function resetCalculator() {
+        const resetButton = document.querySelector(".account-current");
+        resetButton.addEventListener('click', () => display.textContent = "")
+    }
+
+    function erase() {
+        const erase = document.querySelector('[value="Erase"]');
+        const displayText = display.textContent;
+        erase.addEventListener('click', () => {
+            console.log(displayText);
+            displayText.substring(1);
+        })
+    }
+
+    defineDigitsListener();
+    resetCalculator();
+    erase();
 }
-defineDigitsListener();
+init();
